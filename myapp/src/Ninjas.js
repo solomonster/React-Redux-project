e1aaa0b45ddb0ce1efb2a9b305d55df27fdccc83
Ismodon <ismometrical@gmail.com>
2018-12-23 17:02:51 +0300

    New react project

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name: Ismail</div>
+                <div>Age:30</div>
+                <div>Belt: Black</div>
+            </div>
+        );
+    }
+}
+export default Ninjas
